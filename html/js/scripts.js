851d4d20fe3d301d572be1ef307d600f4f06d75b
Eugen-M <geka.manager@gmail.com>
2021-02-28 12:41:00 +0200

    V2

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -1,8 +1,11 @@
 $( document ).ready(function() {
+    // click for btn burger
     $('.header-page__btn-burger').click(function(){
 		$('.header-page__mob-dropdown').toggleClass('header-page__mob-dropdown_opened');
+        $('.header-page__btn-burger').toggleClass('header-page__btn-burger_opened');
 	});
 
+    // review slick slider
     $('.reviews__slider').slick({
         infinite: true,
         slidesToShow: 1,
@@ -12,8 +15,13 @@ $( document ).ready(function() {
         fade: true
     });
 
+    // init for map
     var mapid = L.map('mapid', {
         center: [51.505, -0.09],
         zoom: 13
     });
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapid);
 });
