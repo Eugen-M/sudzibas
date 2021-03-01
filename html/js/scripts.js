4ad54d5dfb82078b205a03ce7829dc91a4c09c89
Eugen-M <geka.manager@gmail.com>
2021-03-01 23:35:10 +0200

    V6

--- html/js/scripts.js
+++ html/js/scripts.js
@@ -5,6 +5,33 @@ $( document ).ready(function() {
         $('.header-page__btn-burger').toggleClass('header-page__btn-burger_opened');
 	});
 
+    // click for lang
+    $('.header-page__mob-dropdown .header-page__lang').click(function(){
+        $(this).toggleClass('header-page__lang_opened');
+	});
+
+    // click for search mobile
+    $('.header-page__search-loupe').click(function(){
+        $('.header-page__search').toggleClass('header-page__search_opened');
+        $('.header-page').toggleClass('header-page_opened-search');
+	});
+
+    // click for navi link Help
+    $('.header-page__menu-item_dropdown .header-page__menu-link').click(function(e){
+        $(this).parent('.header-page__menu-item_dropdown').toggleClass('header-page__menu-item_opened');
+
+        e.stopPropagation();
+        e.preventDefault();
+	});
+
+    // click for navi dropdoqn link Help
+    $('.header-page__navi-link_headphone').click(function(e){
+        $(this).parent('.header-page__navi-item').toggleClass('header-page__navi-item_opened');
+
+        e.stopPropagation();
+        e.preventDefault();
+	});
+
     // review slick slider
     $('.reviews__slider').slick({
         infinite: true,
